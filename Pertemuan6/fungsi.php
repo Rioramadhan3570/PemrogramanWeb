1465c31e6aae2e42b2bf2609662512ecd258f908
Rioramadhan3570 <rramadhan960@gmail.com>
2024-03-18 12:09:19 +0700

    update

--- Pertemuan6/fungsi.php
+++ Pertemuan6/fungsi.php
@@ -4,6 +4,15 @@ function hitungUmur($thn_lahir, $thn_sekarang){
     $umur = $thn_sekarang - $thn_lahir;
     return $umur;
 }
+function perkenalan ($nama, $salam="Assalamuallaikum") {
+    echo $salam.",";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
 
-echo "Umur saya adalah ". hitungUmur(2003, 2024) ." tahun" //isi sesuai dengan tahun lahir kalian
+    //memanggil fungsi lain
+    echo "Saya berusia ". hitungUmur(2003, 2024) ." tahun<br/>";
+    echo "Senang berkenalan dengan anda<br/>";
+}
+
+//memanggil fungsi perkenalan
+    perkenalan ("Elok");
 ?>
